3794d204921be09f2fe7d93a456f275681c3f272
Adam Foster <adampfoster@gmail.com>
2019-03-05 10:01:59 +1000

    Remove API Key

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Firebase
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCNsG4Y4pkaVxqM809bpQeZ3wsFgVlCPcg",
+  apiKey: "Enter your API Key here",
   authDomain: "react-hot-redux-firebase-start.firebaseapp.com",
   databaseURL: "https://react-hot-redux-firebase-start.firebaseio.com",
   storageBucket: "react-hot-redux-firebase-start.appspot.com"
